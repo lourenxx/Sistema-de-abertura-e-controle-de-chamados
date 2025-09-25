b5fe33b2b34b65fc9c16b9e68bb4d8030e238521
lourenxx <guilhermepexiloureco@gmail.com>
2025-09-25 20:32:16 -0300

    Primeiro Commit - Preparando Ambiente

---
+++ SistemaChamados/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SistemaChamados.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
